22fa5baea320e70eca22dbc958aba9aea7c059ca
grigorevadari <d.grig.d.20.04@gmail.com>
2021-03-15 23:48:35 +0300

    feat:array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+exports.min = function min(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    return array.reduce(function (accumulator, currentValue) {
+        if (accumulator < currentValue) {
+            return accumulator;
+        } else {
+            return currentValue;
+        }
+    });
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return array.reduce(function (accumulator, currentValue) {
+        if (accumulator > currentValue) {
+            return accumulator;
+        } else {
+            return currentValue;
+        }
+    });
+};
+
+exports.avg = function avg(array) {
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    const sum = array.reduce(function (accumulator, currentValue) {
+        return accumulator + currentValue;
+    });
+    return sum / array.length;
+};
